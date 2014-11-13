1d340160ff3a6292da999d6d70fe2222b45a2b09
Kenny Lu <klu@avvo.com>
2014-11-12 18:42:09 -0800

    truthy updated and gobble updated

--- turkey.rb
+++ turkey.rb
@@ -1,17 +1,22 @@
 module Animal
   attr_reader :weight
 
+#should report the turkey weight
   def initialize weight
     @weight = weight
   end
 
 end
 
+#should be a kind of animal
 class Turkey
    include Animal
 
-
-  def gobble_speak pleading
-    pleading.gsub!(/[a-z]/, "Gobble")
+#should gobble speak: "Gobble Gobble Gobble gobble Gobble. Gobble Gobb'le Gobble Gobble."
+  def gobble_speak hello
+    hello.gsub!(/\s[A-Z]\s/, " Gobble ");hello.gsub!(/\s[a-z]\s/, " gobble ")
+    hello.gsub!(/[A-Z]\w*/,"Gobble")
+    hello.gsub!(/\s[A-z]+\S[t]\s/, " Gobb'le ");
 end
+
 end
